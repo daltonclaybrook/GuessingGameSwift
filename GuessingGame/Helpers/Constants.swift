f2ecaf5bad81e0e7f748e96d28860b37d3a3a388
Dalton Claybrook <daltonclaybrook@gmail.com>
2022-07-12 09:57:42 -0400

    Update contract addresses and comments

--- GuessingGame/Helpers/Constants.swift
+++ GuessingGame/Helpers/Constants.swift
@@ -2,8 +2,12 @@ import Foundation
 import web3
 
 enum Constants {
-	static let tokenContractAddress = EthereumAddress("0x5Db1436E36cDD5571AA803c2235650eE34fb4Cb4")
-	static let gameContractAddress = EthereumAddress("0x9048bA1fE2b731b3Bd3A8B359545011e63B1da0E")
+	/// The address of the GuessToken contract on Rinkeby
+	static let tokenContractAddress = EthereumAddress("0x2802DF21Ef41951A30BebE49C786E504103cD9f9")
+	/// The address of the GuessingGame contract on Rinkeby
+	static let gameContractAddress = EthereumAddress("0x0D65B3238ceBd5Ed0a17e2D2aBA4485567689156")
+	/// The URL used to access the Ethereum JSON-RPC
 	static let rinkebyURL = URL(string: "https://rinkeby.infura.io/v3/9b881136a0064850aa755873914a946c")!
+	/// The Etherscan API Key used to fetch the gas oracle
 	static let etherscanAPIKey = "YTYHWFBW7TBYJEDTXV7U78FAW9X8UTR1Y9"
 }
